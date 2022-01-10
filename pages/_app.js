2d23448e78229bd4f9d47592a151069820f20388
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2022-01-10 06:15:05 -0500

    removed TS to get rid of lack of types for package

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import Layout from '../components/Layout'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider
       // Provider options are not required but can be useful in situations where
